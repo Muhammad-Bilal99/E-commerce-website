d6d38af35b4d9179897567120c1eddb80d6d3001
Muhammad Bilal <beastop975@gmail.com>
2024-01-03 15:00:22 +0500

    Created navbar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { Children } from "react";
 import {
   createBrowserRouter,
   RouterProvider,
   Outlet,
 } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Product from "./pages/Product/Product";
+import Products from "./pages/Products/Products";
+import "./App.scss"
 
 const Layout = () => {
   return (
